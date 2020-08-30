dcd4de4dd3fd781a6c7a8294f4d4929d04d98128
ahmed_bahaa <a.bahaarefaii@gmail.com>
2020-08-30 23:07:22 +0200

    golang

---
+++ section7/go-practice/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"C:\Users/vegon/Desktop/development/protobuf/section7/go-practice/src/simple/simple.pb.go",
+	"fmt"
+)
+
+func main() {
+	// fmt.Println("hello")
+	doSimple()
+}
+
+func doSimple() {
+	sm := example_simple.SimpleMessage{}
+}
